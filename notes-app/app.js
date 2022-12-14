9047fccd73fe6a4abdd8352dc6c50f6f0a4c5a3d
Akashcitrusbug <akash.citrusbug@gmail.com>
2022-12-14 17:43:17 +0530

    Node Learning

---
+++ notes-app/app.js
@@ -0,0 +1,81 @@
+const yargs = require("yargs");
+// const chalk = require("chalk");
+// const fs =require('fs')
+
+// fs.writeFileSync('notes.txt','This file was created by Node.js')
+
+// fs.appendFileSync('notes.txt', '/nThis file was created by Akash')
+
+// const utils = require('./utils.js')
+
+// console.log(utils);
+
+// const getNotes = require("./notes");
+// const msgGreen = chalk.green("Success!!");
+
+// console.log(msgGreen);
+
+// const chalk = require("chalk");
+// const getNotes = require("./notes.js");
+
+// const msg = getNotes();
+// console.log(msg);
+
+// const greenMsg = chalk.blue.inverse.bold("Success!");
+// console.log(greenMsg);
+
+// console.log(process.argv);
+// console.log(yargs.argv.title);
+// const command = process.argv[2];
+
+// if (command === "add") {
+//   console.log("Adding note");
+// }
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      default: "Default body",
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title = " + argv.title);
+    console.log("Body = " + argv.body);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remvoe a note",
+  handler: function () {
+    console.log("Removing a new note");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a new note",
+  handler: function () {
+    console.log("Reading a new note");
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler: function () {
+    console.log("List notes");
+  },
+});
+
+yargs.parse();
---
+++ notes-app/utils.js
@@ -0,0 +1,5 @@
+console.log("from utils.js");
+
+const my_name = "Akash";
+
+module.exports = my_name;
